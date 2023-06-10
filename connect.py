e56ecbe5451a1eb589051be85e0e3c913a81eac4
YooByeongJu <dbqudwn011@naver.com>
2023-06-10 20:37:20 +0900

    유병주/ 템플렛

--- connect.py
+++ connect.py
@@ -7,14 +7,25 @@ app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['BookDB']
 collection = db['Recd_book_age']
-
+   
 @app.route('/')
 def index():
-    # MongoDB에서 데이터 가져오기
     data = collection.find()
+    return render_template('index.html')
 
-    # HTML 템플릿 렌더링
-    return render_template('index.html', data=data)
+@app.route('/landing')
+def landing():
+    data = collection.find()
+    return render_template('landing.html')
+
+@app.route('/generic')
+def generic():
+    return render_template('generic.html')
+
+@app.route('/elements')
+def elements():
+    return render_template('elements.html')
 
 if __name__ == '__main__':
     app.run()
+    
